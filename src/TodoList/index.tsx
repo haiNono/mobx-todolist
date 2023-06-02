b4ce83c1d7810bf830965413553e22f3e1fef6fe
hainuo.li <hainuo.li@transwarp.io>
2023-06-02 12:19:38 +0800

    feat: 使用全局可观察状态

---
+++ src/TodoList/index.tsx
@@ -0,0 +1,68 @@
+import React, { useContext, useState } from "react";
+import { Store } from "../App";
+import { Card, List, Button, Input } from "antd";
+import { useObserver } from "mobx-react-lite";
+import { toJS } from "mobx";
+import "./index.css";
+
+function TodoList() {
+  const todoStore = useContext(Store);
+  const [title, setTitle] = useState<string>("");
+  const { todos } = todoStore;
+
+  const handleChange = (e: any) => {
+    setTitle(e.target.value);
+  };
+
+  const addTodo = () => {
+    todoStore.addTodo({
+      title,
+      id: Math.random(),
+      done: false,
+    });
+    setTitle("");
+  };
+
+  const delTodo = (id: number) => {
+    return () => {
+      todoStore.delTodo(id);
+    };
+  };
+  console.log(toJS(todoStore.todos));
+
+	// useObserver钩子
+  return useObserver(() => (
+    <div className="wrapper">
+      <Card title="Todo list">
+        <div style={{ display: "flex" }}>
+          <Input
+            placeholder="请输入待办事项"
+            value={title}
+            onChange={handleChange}
+          />{" "}
+          <Button onClick={addTodo}>添加</Button>
+        </div>
+        <List>
+          {todos.map((todo) => (
+            <div
+              key={todo.id}
+              style={{ display: "flex", justifyContent: "space-between" }}
+            >
+              <List.Item>{todo.title}</List.Item>
+              <Button
+                danger
+                size="small"
+                style={{ marginTop: "10px" }}
+                onClick={delTodo(todo.id)}
+              >
+                删除
+              </Button>
+            </div>
+          ))}
+        </List>
+      </Card>
+    </div>
+  ));
+}
+
+export default TodoList;
---
+++ src/store/index.ts
@@ -0,0 +1,41 @@
+import { makeAutoObservable } from "mobx";
+
+interface Todo {
+  done: boolean;
+  title: string;
+  id: number;
+}
+
+class Todos {
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  todos = [
+    { title: "吃饭", done: false, id: Math.random() },
+    { title: "睡觉", done: false, id: Math.random() },
+    { title: "打豆豆", done: true, id: Math.random() },
+  ];
+
+  addTodo = (todo: Todo) => {
+    this.todos.push(todo);
+  };
+
+  delTodo = (id: number) => {
+    this.todos.forEach((item: Todo, index, arr) => {
+      if (item.id === id) {
+        arr.splice(index,1);
+      }
+    });
+  };
+
+  changeDone = (id: number) => {
+    this.todos.forEach((item) => {
+      if (item.id === id) {
+        item.done = !item.done;
+      }
+    });
+  };
+}
+
+export const todoStore = new Todos();

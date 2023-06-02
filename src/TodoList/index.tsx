9ee5268764c965aeb7b3dfe1d8e32ace98b19d0a
hainuo.li <hainuo.li@transwarp.io>
2023-06-02 12:35:09 +0800

    feat: 使用Observe组件创建可观察组件

--- src/TodoList/index.tsx
+++ src/TodoList/index.tsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 import { Store } from "../App";
 import { Card, List, Button, Input } from "antd";
-import { useObserver } from "mobx-react-lite";
+import { Observer } from "mobx-react-lite";
 import { toJS } from "mobx";
 import "./index.css";
 
@@ -28,41 +28,44 @@ function TodoList() {
       todoStore.delTodo(id);
     };
   };
-  console.log(toJS(todoStore.todos));
 
-	// useObserver钩子
-  return useObserver(() => (
-    <div className="wrapper">
-      <Card title="Todo list">
-        <div style={{ display: "flex" }}>
-          <Input
-            placeholder="请输入待办事项"
-            value={title}
-            onChange={handleChange}
-          />{" "}
-          <Button onClick={addTodo}>添加</Button>
-        </div>
-        <List>
-          {todos.map((todo) => (
-            <div
-              key={todo.id}
-              style={{ display: "flex", justifyContent: "space-between" }}
-            >
-              <List.Item>{todo.title}</List.Item>
-              <Button
-                danger
-                size="small"
-                style={{ marginTop: "10px" }}
-                onClick={delTodo(todo.id)}
-              >
-                删除
-              </Button>
+  // useObserver钩子
+  return (
+    <Observer>
+      {() => (
+        <div className="wrapper">
+          <Card title="Todo list">
+            <div style={{ display: "flex" }}>
+              <Input
+                placeholder="请输入待办事项"
+                value={title}
+                onChange={handleChange}
+              />{" "}
+              <Button onClick={addTodo}>添加</Button>
             </div>
-          ))}
-        </List>
-      </Card>
-    </div>
-  ));
+            <List>
+              {todos.map((todo) => (
+                <div
+                  key={todo.id}
+                  style={{ display: "flex", justifyContent: "space-between" }}
+                >
+                  <List.Item>{todo.title}</List.Item>
+                  <Button
+                    danger
+                    size="small"
+                    style={{ marginTop: "10px" }}
+                    onClick={delTodo(todo.id)}
+                  >
+                    删除
+                  </Button>
+                </div>
+              ))}
+            </List>
+          </Card>
+        </div>
+      )}
+    </Observer>
+  );
 }
 
 export default TodoList;
